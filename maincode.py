94e04b21650f54aebec02bb77f5d5ed722c703fc
[your <112771458+Sawaira-0316@users.noreply.github.com>
2023-10-16 22:33:47 +0500

    git add.

--- maincode.py
+++ maincode.py
@@ -1,6 +1,18 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager,Screen
+from kivy.lang import Builder
 
+Builder.load_file('frontend.kv')
+
+class FirstScreen(Screen):
+  def search_image(self):
+    pass
+
+class RootWidget(ScreenManager):
+  pass
 
 class MainApp(App):
-  def __int__(self x,y):
-  self.x=x
+  def build(self):
+    return RootWidget()
+
+MainApp().run()
