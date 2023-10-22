cc428bd6a9c116fefe3af85a33b7e4763ed801c8
[your <112771458+Sawaira-0316@users.noreply.github.com>
2023-10-22 18:42:28 +0500

    git add .

--- maincode.py
+++ maincode.py
@@ -6,7 +6,7 @@ Builder.load_file('frontend.kv')
 
 class FirstScreen(Screen):
   def search_image(self):
-    pass
+   self.manager.current_screen.ids.img.source='files/Pizza.jpg'
 
 class RootWidget(ScreenManager):
   pass
